ee091deddab3fd6a837d92eaf83f18fafa9b65c6
Swapnil05Rai <87640978+Swapnil05Rai@users.noreply.github.com>
2022-12-24 20:51:01 +0530

    Update wisell.py

--- wisell.py
+++ wisell.py
@@ -24,3 +24,15 @@ class Solution:
                     arr[i] = 0
                     arr[j] = ele1 - ele2
         return sum
+
+    
+    """Initialize two variables i and j to 0. These variables will be used to iterate over the houses.
+Start a while loop that will run until either i or j becomes greater than or equal to N.
+Inside the loop, check if the current house at index i has a demand of 0 or if it is a selling house (has a negative demand). If either of these conditions is true, increment i by 1 and continue to the next iteration of the loop.
+If the current house at index i has a positive demand, start another loop that will run until either j becomes greater than or equal to N or the current house at index j becomes a buying house (has a negative demand). Inside this loop, increment j by 1 on each iteration.
+Once the inner loop is finished, calculate the distance between the current house at index i and the buying house at index j as dist = abs(i - j).
+Calculate the amount of wine that the current house at index i is demanding as ele1 = arr[i] * 1. Calculate the amount of wine that the buying house at index j is willing to buy as ele2 = abs(arr[j]) * 1.
+Calculate the total work required to transfer the wine from the buying house at index j to the current house at index i as mul1 = ele1 * dist. Calculate the total work required to transfer the wine from the current house at index i to the buying house at index j as mul2 = ele2 * dist.
+Compare mul1 and mul2 and choose the minimum value. Update the demand of the current house at index i and the buying house at index j accordingly. Add the minimum value to the sum variable.
+Repeat the loop until i or j becomes greater than or equal to N.
+Return the sum variable as the result.""
