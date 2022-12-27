c83f10fe7c80a365c8222fe5b6dfad9e71744643
Swapnil05Rai <87640978+Swapnil05Rai@users.noreply.github.com>
2022-12-27 10:09:59 +0530

    update

--- contwater.py
+++ contwater.py
@@ -10,3 +10,18 @@ def maxArea(A, le):
         else:
             right -= 1
     return max_area
+
+"""The function first initializes three variables:
+
+max_area to store the maximum area found so far, initialized to 0
+left to store the index of the current left line, initialized to the first element of the array
+right to store the index of the current right line, initialized to the last element of the array
+Then, it enters a while loop that continues as long as left is less than right. This is because the two pointers need to be distinct in order to form a valid container.
+
+Inside the loop, the function first calculates the current area between the two lines using the formula min(A[left], A[right]) * (right - left). It then updates the max_area variable if the current area is larger than the maximum area found so far.
+
+After that, the function compares the heights of the two lines and moves the pointer that points to the shorter line towards the other one. This is because the area of the container is determined by the shorter line, so we need to consider other pairs of lines that have a shorter line.
+
+Finally, the function returns the maximum area found after the loop ends.
+
+"""
