6022a5754d28b27cfd4f4b373ecf78485ffc67a1
Swapnil05Rai <87640978+Swapnil05Rai@users.noreply.github.com>
2023-06-16 15:57:53 +0530

    Create MINtime.py

---
+++ MINtime.py
@@ -0,0 +1,18 @@
+from typing import List
+from math import inf
+from collections import defaultdict
+class Solution:
+    def minTime(self, n : int, locations : List[int], types : List[int]) -> int:
+        mn_loc, mx_loc = defaultdict(lambda: inf), defaultdict(lambda: -inf)
+        for typ, loc in zip(types, locations):
+            mn_loc[typ] = min(mn_loc[typ], loc)
+            mx_loc[typ] = max(mx_loc[typ], loc)
+        mn_prev, mx_prev = 0, 0
+        dp = [(0, 0)]
+        for typ in sorted(mn_loc):
+            mn_cur, mx_cur = mn_loc[typ], mx_loc[typ]
+            dp.append((min(dp[-1][0] + abs(mx_cur-mn_prev), dp[-1][1] + abs(mx_cur-mx_prev)) + mx_cur - mn_cur,\
+                min(dp[-1][0] + abs(mn_cur-mn_prev), dp[-1][1] + abs(mn_cur-mx_prev)) + mx_cur - mn_cur))
+            mn_prev, mx_prev =  mn_cur, mx_cur
+            
+        return min(dp[-1][0] + abs(mn_cur), dp[-1][1] + abs(mx_cur))
